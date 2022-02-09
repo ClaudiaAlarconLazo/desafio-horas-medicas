c8453bd1564ff450f69af655dc1ae1723c944a43
claudia_alarcon <c.alarconlazo@gmail.com>
2022-02-09 00:00:17 -0300

    se despliega index con componentes hero, nav, footer, columna principal y card de  bulma

--- assets/js/index.js
+++ assets/js/index.js
@@ -159,67 +159,27 @@ function detail() {
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Functions to open and close a modal
-  function openModal($el) {
-    $el.classList.add('is-active');
-  }
 
-  function closeModal($el) {
-    $el.classList.remove('is-active');
-  }
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-  function closeAllModals() {
-    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
-      closeModal($modal);
-    });
-  }
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
 
-  // Add a click event on buttons to open a specific modal
-  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
-    const modal = $trigger.dataset.target;
-    const $target = document.getElementById(modal);
-    console.log($target);
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
 
-    $trigger.addEventListener('click', () => {
-      openModal($target);
-    });
-  });
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
 
-    // Add a click event on buttons to open a specific modal
-    (document.querySelectorAll('.navbar-burger') || []).forEach(($trigger) => {
-      const modal = $trigger.dataset.target;
-      const $target = document.getElementById(modal);
-      console.log($target);
-  
-      $trigger.addEventListener('click', () => {
-        openModal($target);
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
       });
     });
+  }
 
-  // Add a click event on various child elements to close the parent modal
-  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
-    const $target = $close.closest('.modal');
-
-    $close.addEventListener('click', () => {
-      closeModal($target);
-    });
-  });
-
-    // Add a click event on various child elements to close the parent modal
-    (document.querySelectorAll('.navbar-item, .navbar-end, .navbar-burger, .navbar-menu') || []).forEach(($close) => {
-      const $target = $close.closest('.navbar-menu');
-  
-      $close.addEventListener('click', () => {
-        closeModal($target);
-      });
-    });
-
-  // Add a keyboard event to close all modals
-  document.addEventListener('keydown', (event) => {
-    const e = event || window.event;
-
-    if (e.keyCode === 27) { // Escape key
-      closeAllModals();
-    }
-  });
 });
